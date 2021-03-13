a5ae6a58eb9a7c79ff279c5846a0e758a31bd423
emreturgutce <emrebursalee@gmail.com>
2021-03-13 20:50:30 +0300

    handling multiple connections

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
+	"time"
 )
+
+func handler(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+
+	if err != nil {
+		log.Println("CONN TIMEOUT")
+	}
+
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Code got here.")
+}
 
 func main() {
 	li, err := net.Listen("tcp", ":8080")
@@ -23,10 +44,6 @@ func main() {
 			log.Println(err)
 		}
 
-		io.WriteString(conn, "\nHello from TCP server\n")
-		fmt.Fprintln(conn, "How is your day?")
-		fmt.Fprintf(conn, "%v", "Well, I hope!")
-
-		conn.Close()
+		go handler(conn)
 	}
 }
